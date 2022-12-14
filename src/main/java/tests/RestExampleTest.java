7ff86006658e56687535a8d12cf23053a67c92f6
Dragos Predatu <dragos.predatu@softelligence.net>
2022-12-14 16:17:13 +0200

    initial commit

---
+++ src/main/java/tests/RestExampleTest.java
@@ -0,0 +1,16 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+import io.restassured.response.Response;
+
+public class RestExampleTest {
+	
+	@Test
+	public void postAToDo() {
+		
+		Response response;
+		//https://keytodorestapi.herokuapp.com/
+	}
+
+}
